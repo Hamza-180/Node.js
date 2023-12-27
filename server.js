0e02a90799d8bdc56efd7a899302d27573038c8f
Hamza <hamza.amghar@student.ehb.be>
2023-12-27 20:40:36 +0100

    Delete methode
    Nu kunnen we ook data verwijderen via id.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const Product = require('./models/productModel')
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({extended : false}))
 
 app.get('/',(req,res)=>{
     res.send('hello node  API')
@@ -60,6 +61,20 @@ app.put('/products/:id', async(req, res) => {
         }
         const updatedProduct = await Product.findById(id);
         res.status(200).json(updatedProduct);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.delete('/products/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: `Product is niet gevonden ${id}`})
+        }
+        res.status(200).json(product);
         
     } catch (error) {
         res.status(500).json({message: error.message})
