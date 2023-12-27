d183a6ac5dfeda80c05029fc3eb33cb829380be8
Hamza <hamza.amghar@student.ehb.be>
2023-12-27 19:52:21 +0100

    now you can  fetch data

--- server.js
+++ server.js
@@ -14,8 +14,19 @@ app.get('/blog',(req,res)=>{
     res.send('hello blog my name is hamza')
 })
 
+app.get('/products' , async(req , res )=>{
 
-app.post('/product', async(req, res) =>{
+    try{
+        const products = await Product.find({});
+        res.status(200).json(products);
+    }catch(error){
+
+        res.status(500).json({message: error.message})
+    }
+
+})
+
+app.post('/products', async(req, res) =>{
    try{
     const product = await Product.create(req.body)
     res.status(200).json(product);
