1d1446e7069311ee403b0ee198ccff0706102529
Hamza <hamza.amghar@student.ehb.be>
2023-12-27 18:08:47 +0100

    Install node.js
    Hier heb ik port 3000 gebruikt voor app.listen zodat ik kan zien wat ik doe  bv bij  app.get blog  of app.get
    
    Install nodemon om live te zien update

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ app.get('/',(req,res)=>{
     res.send('hello node  API')
 })
 
+
+app.get('/blog',(req,res)=>{
+    res.send('hello blog my name is hamza')
+})
+
 app.listen(3000, ()=>{
 
 console.log('Node API app  is running on port 3000');
-})
+})  
