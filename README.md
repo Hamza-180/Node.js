8f1c38aa3ae7500a49f5e7af11f60a88e85b7be1
Hamza <hamza.amghar@student.ehb.be>
2023-12-27 19:38:33 +0100

    app.post
    hier heb ik app.post product gemaakt zodat het op mongodb wordt toegevoed
    
    ik doe het op de applicatie insomonia met het formaat json om het te schrijven
    
    ik heb ook mongodb ge instaleert

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Node.js
  API using Node.js
+
+
+https://github.com/Hamza-180/Node.js
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const  mongoose = require('mongoose')
+const Product = require('./models/productModel')
 const app = express()
 
-
+app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.send('hello node  API')
@@ -12,7 +14,31 @@ app.get('/blog',(req,res)=>{
     res.send('hello blog my name is hamza')
 })
 
-app.listen(3000, ()=>{
 
-console.log('Node API app  is running on port 3000');
-})  
+app.post('/product', async(req, res) =>{
+   try{
+    const product = await Product.create(req.body)
+    res.status(200).json(product);
+
+
+   } catch(error){
+    console.log(error.message);
+    res.status(500).json({message: error.message})
+
+   }
+})
+
+
+mongoose.set("strictQuery",false)
+mongoose.
+connect('mongodb+srv://admin:hamza1030@hamzaapi.jqp5uxn.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(()=>{
+    app.listen(3000, ()=>{
+    console.log('Node API app  is running on port 3000');
+    });
+
+    
+}).catch(()=>{
+
+    console.log(error)
+})
